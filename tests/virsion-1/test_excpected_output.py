7736ee24974fdafd01923fd976ba449f07c6502d
adhammhaydat <adhammohidat123@gmail.com>
2021-11-08 10:51:25 +0200

    build: start game feature

--- tests/virsion-1/test_excpected_output.py
+++ tests/virsion-1/test_excpected_output.py
@@ -1,31 +1,33 @@
 from flo import diff
-from tic_tac_toe import *
+from tic_tac_toe.game import  Game
 import pytest
 
+# @pytest.mark.skip()
+
+def test_rules():
+    game = Game()
+    diffs = diff(game.game_rules, path="tests/virsion-1/to_delete.txt")
+    assert not diffs, diffs 
 
 @pytest.mark.skip()
 def test_the_first():
-    game = Game_logic()
-    diffs = diff(game.play, path="test1.txt")
+    game = Game()
+    diffs = diff(game.start_play, path="test1.txt")
     assert not diffs, diffs
 
 
 @pytest.mark.skip()
 def test_second():
-    game = Game_logic()
-    diffs = diff(game.play, path="to_test.sim.txt")
+    game = Game()
+    diffs = diff(game.start_play, path="to_test.sim.txt")
     assert not diffs, diffs
 
 
 @pytest.mark.skip()
 def test_therd():
-    game = Game_logic()
-    diffs = diff(game.play, path="version2.sim.txt")
+    game = Game()
+    diffs = diff(game.start_play, path="version2.sim.txt")
     assert not diffs, diffs
 
 
-@pytest.mark.skip()
-def test_rules():
-    game = Game_logic()
-    diffs = diff(game.play, path="tests/virsion-1/rules.txt")
-    assert not diffs, diffs
+
---
+++ tic_tac_toe/game_logic.py
@@ -0,0 +1,10 @@
+
+class Game_logic():
+    
+    def __init__(self):
+        pass
+    def print_scoreboard(score_board):
+        return "print score board"
+
+    def single_game():
+        pass    
